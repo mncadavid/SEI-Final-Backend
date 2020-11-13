fc8a0ba7e7a4c69bb6dbf818063090043752b042
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-11-13 08:00:41 -0600

    Updated cors options to push to heroku

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const routes = require('./routes');
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
-    // origin: ['http://food-folio.surge.sh/'],
+    // origin: ['http://localhost:3000'],
+    origin: ['http://food-folio.surge.sh/'],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
     optionsSuccessStatus: 200
