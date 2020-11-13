cd316a898888a693478449b20aad78c7b640da04
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-11-13 10:49:56 -0600

    Children to children

--- migrations/20201109211258-create-child.js
+++ migrations/20201109211258-create-child.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Children', {
+    await queryInterface.createTable('children', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Children');
+    await queryInterface.dropTable('children');
   }
 };
