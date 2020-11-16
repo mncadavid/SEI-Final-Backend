a7f010a1d9eec1e916bf1235c7f0e65ab656af91
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-11-16 17:23:24 -0600

    Pushing to heroku

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const routes = require('./routes');
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
-    // origin: ['http://picky-preventer.surge.sh'],
+    // origin: ['http://localhost:3000'],
+    origin: ['http://picky-preventer.surge.sh'],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
     optionsSuccessStatus: 200
